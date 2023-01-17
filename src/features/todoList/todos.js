707acd3d31a9aea5e4810474ae602181fa2be54e
hattima-tim <irfanridwan20@gmail.com>
2023-01-17 04:11:14 +0000

    show dropdown of availabe states

--- src/features/todoList/todos.js
+++ src/features/todoList/todos.js
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 export default function Todo({ todos }) {
+  const [dropDownSelectedFor, setDropDownSelectedFor] = useState("");
+  const [showStateDropDown, setShowStateDropDown] = useState(false);
+
   return (
     <div>
       <table className="min-w-full divide-y-2 divide-gray-200 text-sm">
@@ -27,10 +32,7 @@ export default function Todo({ todos }) {
               </td>
               <td className="whitespace-nowrap px-4 py-2 text-gray-700">
                 <div className="inline-flex items-stretch rounded-md border bg-white dark:border-gray-800 dark:bg-gray-900">
-                  <div
-                    href="/edit"
-                    className="rounded-l-md px-4 py-2 text-sm text-gray-600 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-300 dark:hover:bg-gray-800 dark:hover:text-gray-200"
-                  >
+                  <div className="rounded-l-md px-4 py-2 text-sm text-gray-600 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-300 dark:hover:bg-gray-800 dark:hover:text-gray-200">
                     Todo
                   </div>
 
@@ -38,6 +40,10 @@ export default function Todo({ todos }) {
                     <button
                       type="button"
                       className="inline-flex h-full items-center justify-center rounded-r-md border-l border-gray-100 px-2 text-gray-600 hover:bg-gray-50 hover:text-gray-700 dark:border-gray-700 dark:text-gray-300 dark:hover:bg-gray-800 dark:hover:text-gray-200"
+                      onClick={() => {
+                        setShowStateDropDown(!showStateDropDown);
+                        setDropDownSelectedFor(todo.id);
+                      }}
                     >
                       <span className="sr-only">Menu</span>
                       <svg
@@ -54,56 +60,58 @@ export default function Todo({ todos }) {
                       </svg>
                     </button>
 
-                    <div
-                      className="absolute right-0 z-10 mt-4 w-56 origin-top-right rounded-md border border-gray-100 bg-white shadow-lg dark:border-gray-800 dark:bg-gray-900"
-                      role="menu"
-                    >
-                      <div className="flow-root py-2">
-                        <div className="-my-2 divide-y divide-gray-100 dark:divide-gray-800">
-                          <div className="p-2">
-                            <div
-                              className="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-300"
-                              role="menuitem"
-                            >
-                              In Progress
-                            </div>
-
-                            <div
-                              className="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-300"
-                              role="menuitem"
-                            >
-                              Done
-                            </div>
-
+                    {showStateDropDown && dropDownSelectedFor === todo.id && (
+                      <div
+                        className="absolute right-0 z-10 mt-4 w-56 origin-top-right rounded-md border border-gray-100 bg-white shadow-lg dark:border-gray-800 dark:bg-gray-900"
+                        role="menu"
+                      >
+                        <div className="flow-root py-2">
+                          <div className="-my-2 divide-y divide-gray-100 dark:divide-gray-800">
                             <div className="p-2">
-                              <form method="POST" action="#">
-                                <button
-                                  type="submit"
-                                  className="flex w-full items-center gap-2 rounded-lg px-4 py-2 text-sm text-red-700 hover:bg-red-50 dark:text-red-500 dark:hover:bg-red-600/10"
-                                  role="menuitem"
-                                >
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="h-4 w-4"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                    strokeWidth="2"
+                              <div
+                                className="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-300"
+                                role="menuitem"
+                              >
+                                In Progress
+                              </div>
+
+                              <div
+                                className="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-300"
+                                role="menuitem"
+                              >
+                                Done
+                              </div>
+
+                              <div className="p-2">
+                                <form method="POST" action="#">
+                                  <button
+                                    type="submit"
+                                    className="flex w-full items-center gap-2 rounded-lg px-4 py-2 text-sm text-red-700 hover:bg-red-50 dark:text-red-500 dark:hover:bg-red-600/10"
+                                    role="menuitem"
                                   >
-                                    <path
-                                      strokeLinecap="round"
-                                      strokeLinejoin="round"
-                                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                                    />
-                                  </svg>
-                                  Delete Task
-                                </button>
-                              </form>
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      className="h-4 w-4"
+                                      fill="none"
+                                      viewBox="0 0 24 24"
+                                      stroke="currentColor"
+                                      strokeWidth="2"
+                                    >
+                                      <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                                      />
+                                    </svg>
+                                    Delete Task
+                                  </button>
+                                </form>
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
-                    </div>
+                    )}
                   </div>
                 </div>
               </td>

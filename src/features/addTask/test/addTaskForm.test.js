f0b361504be30da1a0cc31aacea5247557177f8c
hattima-tim <irfanridwan20@gmail.com>
2023-01-19 10:52:48 +0000

    test: a form show up on 'add task' button click

--- src/features/addTask/test/addTaskForm.test.js
+++ src/features/addTask/test/addTaskForm.test.js
@@ -1,8 +1,23 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { toMatchDiffSnapshot } from "snapshot-diff";
 import AddTaskForm from "../addTaskForm";
 
-test('add task button is rendered',()=>{
-    const {container} = render(<AddTaskForm/>);
+expect.extend({ toMatchDiffSnapshot });
 
-    expect(container).toMatchSnapshot();
-})
+test("'add task' button is rendered", () => {
+  const { container } = render(<AddTaskForm />);
+
+  expect(container).toMatchSnapshot();
+});
+
+test("a form show up on 'add task' button click",async()=>{
+    const user = userEvent.setup();
+    const {asFragment} = render(<AddTaskForm/>);
+    const firstRender = asFragment();
+    const addTaskButton = screen.getByRole('button',{name:'Add Task'});
+
+    await user.click(addTaskButton);
+    
+    expect(firstRender).toMatchDiffSnapshot(asFragment());
+});

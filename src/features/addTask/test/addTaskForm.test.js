59db73763331540d26b1c0af1fc241c0a751f519
hattima-tim <irfanridwan20@gmail.com>
2023-01-19 13:08:03 +0000

    test: typing in title field of works

--- src/features/addTask/test/addTaskForm.test.js
+++ src/features/addTask/test/addTaskForm.test.js
@@ -11,13 +11,27 @@ test("'add task' button is rendered", () => {
   expect(container).toMatchSnapshot();
 });
 
-test("a form show up on 'add task' button click",async()=>{
-    const user = userEvent.setup();
-    const {asFragment} = render(<AddTaskForm/>);
-    const firstRender = asFragment();
-    const addTaskButton = screen.getByRole('button',{name:'Add Task'});
+test("a form show up on 'add task' button click", async () => {
+  const user = userEvent.setup();
+  const { asFragment } = render(<AddTaskForm />);
+  const firstRender = asFragment();
+  const addTaskButton = screen.getByRole("button", { name: "Add Task" });
 
-    await user.click(addTaskButton);
-    
-    expect(firstRender).toMatchDiffSnapshot(asFragment());
+  await user.click(addTaskButton);
+
+  expect(firstRender).toMatchDiffSnapshot(asFragment());
+});
+
+test("typing in title field of 'add task form' works", async () => {
+  const user = userEvent.setup();
+  render(<AddTaskForm />);
+  const addTaskButton = screen.getByRole("button", { name: "Add Task" });
+
+  await user.click(addTaskButton);
+  const titleField = screen.getByRole("textbox", {
+    name: /title/i,
+  });
+  await user.type(titleField, "test task");
+
+  expect(titleField).toHaveValue("test task");
 });
